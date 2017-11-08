febb3b44f4278713ace3b81b47e175fac6450e99
PELLE <estelle.pelle.external@atos.net>
2017-11-08 12:08:44 +0100

    directive, filtre, valide formulaire

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,53 @@
  * Controller of the desktopApp
  */
 angular.module('desktopApp')
-    .controller('MainCtrl', function () {
+    .controller('MainCtrl', ['$scope', function ($scope) {
         this.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
+
+        $scope.tabPersonne = [{
+            nom: "meerbeck",
+            prenom: "heidi"
+            }, {
+            nom: "hendrickx",
+            prenom: "jan"
+            }, {
+            nom: "descheemaeker",
+            prenom: "luc"
+            }, {
+            nom: "clohse",
+            prenom: "sylvia"
+            }];
+        
+        $scope.savePersonne = function(){
+            console.log($scope.ajoutPersonne.nom.$invalid);
+        };
+    }])
+    .directive('formulaire', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '../views/formulaire.html'
+        };
+    })
+    .directive('tableauFiltre', function(){
+        return {
+            restrict: 'E',
+            templateUrl: '../views/tableauFiltre.html'
+        };
+    }).filter('filterC', function () {
+        return function (input, uppercase) {
+            input = input || '';
+            var out = '';
+            for (var i = 0; i < input.length; i++) {
+                out = input.charAt(i) + out;
+            }
+            // conditional based on optional argument
+            if (uppercase) {
+                out = out.toUpperCase();
+            }
+            return out;
+        };
     });
