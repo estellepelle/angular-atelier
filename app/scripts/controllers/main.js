496bb9cd5657c4e8e0de84aa67170b664fdb31dd
PELLE <estelle.pelle.external@atos.net>
2017-11-08 16:28:57 +0100

    service get fichier json et post

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,35 +8,96 @@
  * Controller of the desktopApp
  */
 angular.module('desktopApp')
-    .controller('MainCtrl', ['$scope', function ($scope) {
+    .controller('MainCtrl', ['$scope', '$http', '$uibModal', function ($scope, $http, $uibModal) {
         this.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
+        
+        $scope.personne = {
+            nom : null,
+            prenom : null,
+            telephone : null,
+            mail : null,
+            montant : null
+        };
 
         $scope.tabPersonne = [{
             nom: "meerbeck",
-            prenom: "heidi"
+            prenom: "heidi",
+            mail : "hm@g",
+            montant : "2600",
+            telephone :"0247256985"
             }, {
             nom: "hendrickx",
-            prenom: "jan"
+            prenom: "jan",
+            mail : "jh@g",
+            montant : "3500",
+            telephone :"0247859636"
             }, {
             nom: "descheemaeker",
-            prenom: "luc"
+            prenom: "luc",
+            mail : "ld@g",
+            montant : "1350",
+            telephone :"0245478590"
             }, {
             nom: "clohse",
-            prenom: "sylvia"
+            prenom: "sylvia",
+            mail : "sc@g",
+            montant : "1960",
+            telephone :"0247369856"
             }];
         
         $scope.savePersonne = function(){
             console.log($scope.ajoutPersonne.nom.$invalid);
+            console.log($scope.personne);
+            
+            if(!$scope.ajoutPersonne.nom.$invalid && !$scope.ajoutPersonne.prenom.$invalid&& !$scope.ajoutPersonne.telephone.$invalid  && !$scope.ajoutPersonne.mail.$invalid  && !$scope.ajoutPersonne.montant.$invalid ){
+                $scope.tabPersonne.push($scope.personne);
+            }
+            
+            
         };
+        
+        $scope.envoyerInfo = function () {
+            $http({
+                method: 'POST',
+                url: '',
+                data: $scope.personne
+            }).then(function successCallback(response) {
+                $scope.DocEnvoye = response.data.$scope.personne;
+                window.open("data:application/pdf;base64," + response.data.$scope.personne);
+            }, function errorCallback(response) {
+                if(response.status === 404){
+                    
+                    var modalInstance = $uibModal.open({
+                        templateUrl: '../../404.html'
+                    })
+                }
+                
+            });
+        };
+        
+        
+          $scope.recupInfo = function () {
+            $http({
+                method: 'GET',
+                url: '../../fiche.json'
+            }).then(function successCallback(response) {
+                $scope.DocRecup = response;
+                console.log($scope.DocRecup);
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        };
+      
+
     }])
     .directive('formulaire', function () {
         return {
             restrict: 'E',
-            templateUrl: '../views/formulaire.html'
+            templateUrl: '../../views/formulaire.html'
         };
     })
     .directive('tableauFiltre', function(){
@@ -58,3 +119,4 @@ angular.module('desktopApp')
             return out;
         };
     });
+
